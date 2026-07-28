58e9d0fe6f199e22efa040e47d0b33552f5f9533
Temple7447 <starukido@gmail.com>
2026-07-28 16:08:47 +0500

    new

---
+++ script.js
@@ -0,0 +1,166 @@
+
+
+
+
+
+
+console.log("there is an issue there in line 100")
+
+   
+    const a = 2;
+    const b  = 4;
+    let c = 3;
+    
+
+ const x = (-b - (b^2-4*a*c)^(1/2))/2*a
+
+ c= 8
+
+
+ let first_name = `VOKE`
+ let last_name = "VOKE"
+
+ first_name = "JOHN"
+ first_name = "SAMSOM"
+
+
+  const first_name1  = "TEMPLE" 
+ const last_name1 = "VOKE"
+
+
+
+
+ 
+
+
+
+ console.log("let", first_name,  last_name)
+ console.log("const", first_name1,  last_name1)
+
+ document.getElementById("buttonname").textContent =  `${first_name} ${last_name}`
+ document.getElementById("answer").textContent =  c
+
+
+//  document.getElementById("body").style.backgroundColor = "blue"
+//  document.getElementById("container").style.width = "400px"
+//  document.getElementById("container").style.height = "400px"
+//  document.getElementById("container").style.backgroundColor = "green"
+
+ let age = 18; 
+ let male = 18;  
+ let female = 16;
+
+
+// if(age < 20){
+//   console.log("age is less than 20")
+//   document.getElementById("conditionanswer").textContent = "age is less than 20"
+// }else{
+// console.log("age is not less than 20")
+//   document.getElementById("conditionanswer").textContent = "age is not less than 20"
+// }
+
+
+// if(male => 18){
+//       console.log("male should move to class A")
+// }else{
+//       console.log("male should move to class B")
+// }
+
+
+
+
+
+// if(male >  18 && female < 16){
+//   console.log("male should move to class A")
+//   document.getElementById("conditionanswer").textContent = "male should move to class A"
+// }else{
+// console.log("male should move to class B")
+//   document.getElementById("conditionanswer").textContent = "male should move to class A"
+// }
+
+
+//  switch(condition){
+//   case 
+
+//   case 
+//   case
+//   case
+
+
+//   break;
+
+//   default
+    
+//  }
+
+// const today = "MONDAY"
+
+
+// switch(today){
+//   case "TUESDAY":
+//     console.log("Today is tuesday, welcome sir/ma");
+//     break;
+//   case "MONDAY":
+//     console.log("Today is MONDAY, welcome sir/ma");
+//     break;
+//   case "SUNDAY":
+//     console.log("Today is SUNDAY, welcome sir/ma")
+//     break;
+//   default:
+//     console.log("welcome sir/ma")
+// }
+
+
+
+function functionName(){
+
+
+
+  
+    // return console.log("this is a function");
+    return console.log(a);
+
+}
+
+
+// const functionName = ()=>{
+
+
+
+  functionName()
+
+//   return
+// }
+
+
+
+
+
+
+
+const ages =document.getElementById("inputvalue").value;
+console.log(ages, ",,,,,,,,,,,,,,,,,,,,,")
+
+functionName()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
